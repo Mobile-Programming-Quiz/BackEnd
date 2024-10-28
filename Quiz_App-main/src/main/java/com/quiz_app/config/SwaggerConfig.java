3ace8e4acf7fccf9f81d117267d84f827d424420
seungjae708 <choesj365@gmail.com>
2024-10-28 17:41:14 +0900

    FEAT: 스웨거 config 추가

---
+++ Quiz_App-main/src/main/java/com/quiz_app/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.quiz_app.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Quiz App API")
+                        .description("API documentation for Quiz Application")
+                        .version("v1.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Quiz App Documentation")
+                        .url("https://github.com/your-repo-url"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
